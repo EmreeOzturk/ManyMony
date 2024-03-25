c0dd04b63f01203c7f504b5c0e716ac3d5b1be7d
Emre Ozturk <emreeozturk42@gmail.com>
2024-03-25 10:17:36 +0300

    Refactor DashboardLoader to include expenses

--- src/loaders/DashboardLoader.ts
+++ src/loaders/DashboardLoader.ts
@@ -2,8 +2,9 @@ import { fetchDataFromLocalStorage } from "../helper";
 export const dashboardLoader = () => {
   const userName = fetchDataFromLocalStorage("userName");
   const budgets = fetchDataFromLocalStorage("budgets");
-  if (userName || budgets) {
-    return { userName, budgets };
+  const expenses = fetchDataFromLocalStorage("expenses");
+  if (userName || budgets || expenses) {
+    return { userName, budgets, expenses };
   }
   return { userName: "" };
 };
