678992f2b6dc0f49bca8cdb7c007471214c5af02
Emre Ozturk <emreeozturk42@gmail.com>
2024-03-25 11:10:22 +0300

    Refactor BudgetItem component to include total expenses and progress bar

--- src/components/BudgetItem.tsx
+++ src/components/BudgetItem.tsx
@@ -1,11 +1,30 @@
+import { formatCurrency, calculateTotalExpensesByBudgetId, formatPercentage } from "../helper"
 import { Budget } from "../types"
 
 const BudgetItem = (
     { budget }: { budget: Budget }
 ) => {
+    const { id, name, amount, color } = budget;
+    const totalExpense = calculateTotalExpensesByBudgetId(id);
     return (
-        <div>
-            
+        <div className="budget"
+            style={{
+                borderColor: color,
+                color: color,
+                backgroundColor: `${color}1A`
+            }}
+        >
+            <div className="progress-text">
+                <h3>{name}</h3>
+                <p>{formatCurrency(amount)}</p>
+            </div>
+            <progress max={amount} value={totalExpense} >
+                {formatPercentage(amount, totalExpense)}
+            </progress>
+            <div className="progress-text">
+                <small>{formatCurrency(totalExpense)} spent</small>
+                <small>{formatCurrency(amount - totalExpense)} remaining</small>
+            </div>
         </div>
     )
 }
