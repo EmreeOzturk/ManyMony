02d6e0e7ed68c0f63c81ca7e2fe78526f18591ff
Emre Ozturk <emreeozturk42@gmail.com>
2024-03-25 10:17:57 +0300

    Add BudgetItem component

---
+++ src/components/BudgetItem.tsx
@@ -0,0 +1,13 @@
+import { Budget } from "../types"
+
+const BudgetItem = (
+    { budget }: { budget: Budget }
+) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default BudgetItem
