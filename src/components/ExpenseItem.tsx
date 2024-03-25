c0f70171476b351c263c0a44143858eaba6b8f58
Emre Ozturk <emreeozturk42@gmail.com>
2024-03-25 14:02:36 +0300

    Add budget name and delete button to ExpenseItem component

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,14 +1,30 @@
-import { formatCurrency, formatDateToLocaleString } from "../helper"
+import { formatCurrency, formatDateToLocaleString, findBudgetPropsById } from "../helper"
 import { Expense } from "../types"
-
+import { useLocation, Link } from 'react-router-dom'
+import Trash from "../assets/icons/Trash"
 const ExpenseItem = ({ expense }
     : { expense: Expense }
 ) => {
+    const location = useLocation();
     return (
         <>
             <td>{expense.name}</td>
             <td>{formatCurrency(expense.amount)}</td>
             <td>{formatDateToLocaleString(expense.createdAt)}</td>
+            {location.pathname === "/expenses" && (<td >
+                <Link to={`/`}
+                    style={{
+                        backgroundColor: findBudgetPropsById(expense.budgetId)?.color
+                    }}
+                >
+                    {findBudgetPropsById(expense.budgetId)?.name}
+                </Link>
+            </td>)}
+            {location.pathname === "/expenses" && (<td className="text-right">
+                <button className="btn btn--warning">
+                    <Trash />
+                </button>
+            </td>)}
         </>
     )
 }
