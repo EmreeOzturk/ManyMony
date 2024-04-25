a684d028eb7b120791b997c850bab0ee4c77a415
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-26 02:42:04 +0300

    Fix import issue in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import { Outlet, useLoaderData } from "react-router-dom"
-import wave from "./assets/wave.svg"
+// import wave from "./assets/wave.svg"
 import Header from "./components/Header";
 function App() {
   const { userName } = useLoaderData() as { userName: string };
   return (
-    <div className="layout">
+    <
+    // className="layout"
+    >
       <Header userName={userName} />
       <main>
         <Outlet />
       </main>
       <footer>
-        <img src={wave} alt="wave" />
+        {/* <img src={wave} alt="wave" /> */}
       </footer>
-    </div>
+    </>
   )
 }
 
