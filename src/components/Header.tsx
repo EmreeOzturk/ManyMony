803502fbce3b7af563ad3fa261e5d7f56aab0559
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-26 02:41:52 +0300

    Update Header component with new logo and navigation links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,39 +5,53 @@ const Header = (
     { userName }: { userName: string }
 ) => {
     return (
-        <nav>
-            <NavLink to="/" aria-label='Go to home'>
-                <img src={logomark} alt="logomark" />
-                <span>
-                    Many Mony
-                </span>
-            </NavLink>
-            {
-                userName ? (
-                    <Form action='/logout' method='POST'
-                        onSubmit={(event) => {
-                            if (!confirm('Are you sure you want to logout?')) {
-                                event.preventDefault()
-                            }
-                        }
-                        }>
-                        <button
-                            type='submit'
-                            aria-label='Logout'
-                            className='btn btn--warning'
-                        >
-                            <Trash />
-                            Logout
-                        </button>
-                    </Form>
-                ) : (
-                    <div >
+        <header className='bg-slate-600/40 text-white max-w-7xl fixed left-0 right-0 mt-4 rounded-xl shadow-lg mx-auto flex justify-between items-center'>
+            <div>
+                Logo
+            </div>
 
-                    </div>
-                )
-            }
-        </nav >
+            <div>
+                <nav>
+                    <p>Login</p>
+                    <p>Register</p>
+                </nav>
+            </div>
+        </header>
     )
 }
 
 export default Header
+
+
+{/* <nav>
+<NavLink to="/" aria-label='Go to home'>
+    <img src={logomark} alt="logomark" />
+    <span>
+        Many Mony
+    </span>
+</NavLink>
+{
+    userName ? (
+        <Form action='/logout' method='POST'
+            onSubmit={(event) => {
+                if (!confirm('Are you sure you want to logout?')) {
+                    event.preventDefault()
+                }
+            }
+            }>
+            <button
+                type='submit'
+                aria-label='Logout'
+                className='btn btn--warning'
+            >
+                <Trash />
+                Logout
+            </button>
+        </Form>
+    ) : (
+        <div >
+
+        </div>
+    )
+}
+</nav > */}
