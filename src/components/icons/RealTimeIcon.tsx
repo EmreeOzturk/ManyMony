6266ec6dbfac98e8fed96905ec65413ab6fbc2fe
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-26 06:24:07 +0300

    Add new SVG icons for Chart, RealTime, Tag, and Wallet

---
+++ src/components/icons/RealTimeIcon.tsx
@@ -0,0 +1,8 @@
+
+const RealTimeIcon = () => {
+    return (
+        <svg stroke="#42219A" fill="none" stroke-width="2"  viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 16l6 -7l5 5l5 -6"></path><path d="M15 14m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path><path d="M10 9m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path><path d="M4 16m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path><path d="M20 8m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path></svg>
+    )
+}
+
+export default RealTimeIcon
