a96bc5009b9984b98934c036d5456dc77d09d144
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-26 05:21:27 +0300

    Update LangingPage component with motion animations and styles

--- src/pages/LangingPage.tsx
+++ src/pages/LangingPage.tsx
@@ -1,25 +1,47 @@
 import { motion } from "framer-motion";
 import { LampContainer } from "../components/ui/lamp";
+import { HoverBorderGradient } from "../components/ui/hover-border-gradient";
 const LangingPage = () => {
   return (
     <>
-      <section className="h-screen absolute inset-0 -z-10  w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-        <LampContainer
+      {/* <section className="h-full absolute inset-0 -z-10  w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"> */}
+      <LampContainer>
+        <motion.div
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl "
         >
-          <motion.h1
-            initial={{ opacity: 0.5, y: 100 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{
-              delay: 0.3,
-              duration: 0.8,
-              ease: "easeInOut",
-            }}
-            className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+          <h1>A New Way to Budget</h1>
+          <h2 className="text-5xl mt-2">Easy, Fast, and Effective</h2>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className=" bg-gradient-to-br flex gap-3 from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent"
+        >
+          <HoverBorderGradient
+            containerClassName="rounded-full"
+            as="button"
+            className="bg-black text-white flex items-center space-x-2"
           >
-            Build lamps <br /> the right way
-          </motion.h1>
-        </LampContainer>
-      </section>
+            Get Started
+          </HoverBorderGradient>
+        </motion.div>
+      </LampContainer>
+      <div className="h-screen">
+
+      </div>
+      {/* </section> */}
     </>
   )
 }
