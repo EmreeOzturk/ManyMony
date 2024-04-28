318f9de8487389d301280ce9b22910167bacb75f
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-28 09:13:11 +0300

    Update RegisterPage component with UI changes

--- src/pages/auth/RegisterPage.tsx
+++ src/pages/auth/RegisterPage.tsx
@@ -2,7 +2,7 @@ import { Input } from "../../components/ui/input"
 import { BackgroundBeams } from "../../components/ui/background-beams";
 import { cn } from "../../utils/cn";
 import { useMotionTemplate, useMotionValue, motion } from "framer-motion";
-import {useState} from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 const BottomGradient = () => {
 
@@ -29,7 +29,7 @@ const RegisterPage = () => {
     }
     return (
         <div className="h-screen w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-            <div className="w-[600px] z-20  mx-auto p-4">
+            <div className="w-[600px] z-20 mx-auto p-4">
                 <motion.form
                     style={{
                         background: useMotionTemplate`
@@ -60,34 +60,47 @@ const RegisterPage = () => {
                             Fill in the form to get started
                         </p>
                     </div>
-                    <Input id="email" placeholder="youremail@manymony.com" type="email" />
-                    <Input id="password" placeholder="Your Password" type="password" />
+                    <div className="w-full">
+                        <label className="text-neutral-400" htmlFor="email">Name</label>
+                        <Input id="name" placeholder="emre öztürk" type="text" />
+                    </div>
+                    <div className="w-full">
+                        <label className="text-neutral-400" htmlFor="email">Email</label>
+                        <Input id="email" placeholder="youremail@manymony.com" type="email" />
+                    </div>
+                    <div className="w-full">
+                        <label htmlFor="password" className="text-neutral-400">Password</label>
+                        <Input id="password" placeholder="*********" type="password" />
+                    </div>
+
+                    <div className="w-full">
+                        <label htmlFor="password" className="text-neutral-400">Confirm Password</label>
+                        <Input id="password" placeholder="*********" type="password" />
+                    </div>
+
                     <button
-                        className="bg-gradient-to-br relative group/btn from-black  to-neutral-600 block  w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+                        className="bg-gradient-to-br relative group/btn from-black  to-neutral-800 block  w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
                         type="submit"
                     >
                         <span
                             className=""
                         >
-                            Log in
+                            Register
                         </span>
                         <BottomGradient />
                     </button>
                     <div
                         className="flex flex-col items-start justify-center w-full text-center mt-4"
                     >
-                        <Link to="#" className="text-neutral-400 dark:text-neutral-700
-              hover:text-neutral-500 hover:underline
-            ">Forgot your password?</Link>
-                        <Link to="/register" className="text-neutral-400 dark:text-neutral-700 mt-2 hover:text-neutral-500 hover:underline">Don't have an account? {" "}
+                        <Link to="/login" className="text-neutral-400 dark:text-neutral-700 hover:text-neutral-500 hover:underline">Already have an account? {" "}
                             <span
                                 className="font-bold"
                             >
-                                Register
+                                Log in
                             </span>
                         </Link>
                     </div>
-                    <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+                    <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent mt-8 h-[1px] w-full" />
                 </motion.form>
             </div>
             <BackgroundBeams />
