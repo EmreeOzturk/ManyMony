71cf4ad70c1e6a1764bc48a78bc9d57a9e4e31ae
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-29 04:57:21 +0300

    Refactor appwriteConfig.js to remove unused code and update configuration

--- src/appwriteConfig.js
+++
@@ -1,7 +0,0 @@
-import { Client, Account } from "appwrite";
-
-export const API_ENDPOINT = "https://cloud.appwrite.io/v1";
-export const PROJECT_ID = "662efab30012ddca3221";
-const client = new Client().setEndpoint(API_ENDPOINT).setProject(PROJECT_ID);
-export const account = new Account(client);
-export default client;
