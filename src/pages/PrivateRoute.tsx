1050ed0097e355a149475f64ca4ee433a39770f6
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-29 08:22:21 +0300

    Refactor PrivateRoute component to use the useAuth hook for authentication

--- src/pages/PrivateRoute.tsx
+++ src/pages/PrivateRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../context/AuthProvider";
+import { useAuth } from '../hooks/useAuth'
 import { Models } from "appwrite";
 const PrivateRoute = () => {
     const auth = useAuth() as { user: Models.Session | null };
