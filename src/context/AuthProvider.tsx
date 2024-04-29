2345888c12c348928a1756d25eb2c493e64603d5
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-29 05:46:52 +0300

    Refactor AuthProvider.tsx to improve login functionality and error handling

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,4 +1,9 @@
 import { createContext, useContext, useState } from "react";
+import { emailRegex, passwordRegex } from "../helper/consts";
+import { account } from "../lib/appwrite";
+import { toast } from "react-toastify";
+import { redirect } from "react-router-dom";
+
 interface defaultAuthContext {
     user: string | null;
     setUser?: (user: string) => void;
@@ -16,6 +21,33 @@ const AuthContext = createContext<defaultAuthContext | undefined>(defaultAuthCon
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<string | null>(null)
     const [token, setToken] = useState(localStorage.getItem("token") || "");
+
+    const loginAction = async (email: string, password: string) => {
+        try {
+            if (email === "" || password === "") {
+                throw new Error("Please provide a valid input");
+            }
+
+            if (!emailRegex.test(email as string)) {
+                throw new Error("Please provide a valid email");
+            }
+
+            if (!passwordRegex.test(password as string)) {
+                throw new Error("Please provide a valid password");
+            }
+
+            const result = await account.createEmailPasswordSession(
+                email as string,
+                password as string
+            );
+
+            toast.success("Login successful");
+            return redirect("/");
+        } catch (error) {
+            toast.error((error as Error).message);
+            throw new Error((error as Error).message);
+        }
+    }
 
     return (
         <AuthContext.Provider value={
