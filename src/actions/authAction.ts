9e793f109b618e982e36b8226b4418b9267ab688
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-29 06:27:03 +0300

    Refactor authAction.ts to remove unused code and integrate Appwrite API for login functionality

--- src/actions/authAction.ts
+++ src/actions/authAction.ts
@@ -2,7 +2,6 @@ import type { ActionFunction } from "react-router-dom";
 import { toast } from "react-toastify";
 import { redirect } from "react-router-dom";
 import { emailRegex, passwordRegex } from "../helper/consts";
-import { account } from "../lib/appwrite";
 
 type authActionProps = {
   request: Request;
@@ -12,35 +11,7 @@ export const authAction: ActionFunction<authActionProps> = async ({
 }) => {
   const formData = await request.formData();
   const actionType = formData.get("_action");
-  if (actionType === "login") {
-    try {
-      const email = formData.get("email");
-      const password = formData.get("password");
 
-      if (email === "" || password === "") {
-        throw new Error("Please provide a valid input");
-      }
-
-      if (!emailRegex.test(email as string)) {
-        throw new Error("Please provide a valid email");
-      }
-
-      if (!passwordRegex.test(password as string)) {
-        throw new Error("Please provide a valid password");
-      }
-
-      const result = await account.createEmailPasswordSession(
-        email as string,
-        password as string
-      );
-
-      toast.success("Login successful");
-      return redirect("/");
-    } catch (error) {
-      toast.error((error as Error).message);
-      throw new Error((error as Error).message);
-    }
-  }
   if (actionType === "register") {
     try {
       const email = formData.get("email");
