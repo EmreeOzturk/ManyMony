724d2e74dbaa7c5b8d25ce2d5c128ad1bc31b185
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-29 04:09:19 +0300

    Refactor AuthProvider component to include setUser and setToken functions

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,10 +1,11 @@
 import { createContext, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 
 interface defaultAuthContext {
     user: string | null;
+    setUser?: (user: string) => void;
     token: string;
+    setToken?: (token: string) => void;
 }
 
 const defaultAuthContext: defaultAuthContext = {
@@ -17,48 +18,15 @@ const AuthContext = createContext<defaultAuthContext | undefined>(defaultAuthCon
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<string | null>(null)
     const [token, setToken] = useState(localStorage.getItem("site") || "");
-    const navigate = useNavigate()
 
-    const loginAction = async () => {
-        try {
-            const response = await fetch("your-api-endpoint/auth/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                // body: JSON.stringify(data),
-            });
-            const res = await response.json();
-            if (res.data) {
-                setUser(res.data.user);
-                setToken(res.token);
-                localStorage.setItem("site", res.token);
-                navigate("/dashboard");
-                return;
-            }
-            throw new Error(res.message);
-        } catch (err) {
-            console.error(err);
-        }
-    }
-
-    const logoutAction = async () => {
-        try {
-            localStorage.removeItem("site");
-            setUser(null);
-            setToken("");
-            navigate("/login");
-        } catch (err) {
-            console.error(err);
-        }
-    }
 
     return (
         <AuthContext.Provider value={
             {
                 user,
                 token,
-                logoutAction
+                setUser,
+                setToken
             }
         }>
             {children}
