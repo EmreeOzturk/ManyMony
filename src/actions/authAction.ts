05cac9a18bc64c7db11c95721acf336c9289f9f7
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-29 04:26:23 +0300

    Refactor authAction.ts to improve error handling and add form validation

--- src/actions/authAction.ts
+++ src/actions/authAction.ts
@@ -11,26 +11,21 @@ export const authAction: ActionFunction<authActionProps> = async ({
 }) => {
   const formData = await request.formData();
   const actionType = formData.get("_action");
-  console.log(request.url);
   if (actionType === "login") {
     try {
-      console.log("geldi");
       const email = formData.get("email");
       const password = formData.get("password");
 
       if (email === "" || password === "") {
-        alert("Please provide a valid input");
-        // throw new Error("Please provide a valid input");
+        throw new Error("Please provide a valid input");
       }
 
       if (!emailRegex.test(email as string)) {
-        alert("Please provide a valid email");
-        // throw new Error("Please provide a valid email");
+        throw new Error("Please provide a valid email");
       }
 
       if (!passwordRegex.test(password as string)) {
-        alert("Please provide a valid password");
-        // throw new Error("Please provide a valid password");
+        throw new Error("Please provide a valid password");
       }
 
       // call login action
@@ -40,44 +35,44 @@ export const authAction: ActionFunction<authActionProps> = async ({
       return redirect("/");
     } catch (error) {
       toast.error((error as Error).message);
-      //   throw new Error((error as Error).message);
+      throw new Error((error as Error).message);
     }
   }
-  //   if (actionType === "register") {
-  //     try {
-  //       const email = formData.get("email");
-  //       const password = formData.get("password");
+  if (actionType === "register") {
+    try {
+      const email = formData.get("email");
+      const password = formData.get("password");
 
-  //       if (email === "" || password === "") {
-  //         throw new Error("Please provide a valid input");
-  //       }
+      if (email === "" || password === "") {
+        throw new Error("Please provide a valid input");
+      }
 
-  //       if (!emailRegex.test(email as string)) {
-  //         throw new Error("Please provide a valid email");
-  //       }
+      if (!emailRegex.test(email as string)) {
+        throw new Error("Please provide a valid email");
+      }
 
-  //       if (!passwordRegex.test(password as string)) {
-  //         throw new Error("Please provide a valid password");
-  //       }
-  //       // call register action
-  //       //   registerAction();
-  //       // appwrite api endpoint
-  //       toast.success("Register successful");
-  //       return redirect("/login");
-  //     } catch (error) {
-  //       toast.error((error as Error).message);
-  //       throw new Error((error as Error).message);
-  //     }
-  //   }
+      if (!passwordRegex.test(password as string)) {
+        throw new Error("Please provide a valid password");
+      }
+      // call register action
+      //   registerAction();
+      // appwrite api endpoint
+      toast.success("Register successful");
+      return redirect("/login");
+    } catch (error) {
+      toast.error((error as Error).message);
+      throw new Error((error as Error).message);
+    }
+  }
 
-  //   if (actionType === "logout") {
-  //     try {
-  //       // call logout action
-  //       //   logoutAction();
-  //       return redirect("/login");
-  //     } catch (error) {
-  //       toast.error((error as Error).message);
-  //       throw new Error((error as Error).message);
-  //     }
-  //   }
+  if (actionType === "logout") {
+    try {
+      // call logout action
+      //   logoutAction();
+      return redirect("/login");
+    } catch (error) {
+      toast.error((error as Error).message);
+      throw new Error((error as Error).message);
+    }
+  }
 };
