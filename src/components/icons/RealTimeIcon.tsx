7f15aef786772ba44f27a387c248cb082d31bc6a
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-29 04:14:32 +0300

    Refactor SVG stroke attributes to use strokeLinecap instead of stroke-linecap

--- src/components/icons/RealTimeIcon.tsx
+++ src/components/icons/RealTimeIcon.tsx
@@ -1,7 +1,7 @@
 
 const RealTimeIcon = () => {
     return (
-        <svg stroke="#722020" fill="none" strokeWidth="2"  viewBox="0 0 24 24" stroke-linecap="round" strokeLinejoin="round" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 16l6 -7l5 5l5 -6"></path><path d="M15 14m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path><path d="M10 9m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path><path d="M4 16m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path><path d="M20 8m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path></svg>
+        <svg stroke="#722020" fill="none" strokeWidth="2"  viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 16l6 -7l5 5l5 -6"></path><path d="M15 14m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path><path d="M10 9m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path><path d="M4 16m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path><path d="M20 8m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path></svg>
     )
 }
 
