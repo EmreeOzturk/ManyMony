8440403330b48d4c66478f471726724c4d8fcccb
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-29 04:09:23 +0300

    Refactor authAction.ts to improve error handling and add toast notifications

--- src/actions/authAction.ts
+++ src/actions/authAction.ts
@@ -2,6 +2,7 @@ import type { ActionFunction } from "react-router-dom";
 import { toast } from "react-toastify";
 import { redirect } from "react-router-dom";
 import { emailRegex, passwordRegex } from "../helper/consts";
+
 type authActionProps = {
   request: Request;
 };
@@ -10,64 +11,73 @@ export const authAction: ActionFunction<authActionProps> = async ({
 }) => {
   const formData = await request.formData();
   const actionType = formData.get("_action");
-
+  console.log(request.url);
   if (actionType === "login") {
     try {
+      console.log("geldi");
       const email = formData.get("email");
       const password = formData.get("password");
 
       if (email === "" || password === "") {
-        throw new Error("Please provide a valid input");
+        alert("Please provide a valid input");
+        // throw new Error("Please provide a valid input");
       }
 
       if (!emailRegex.test(email as string)) {
-        throw new Error("Please provide a valid email");
+        alert("Please provide a valid email");
+        // throw new Error("Please provide a valid email");
       }
 
       if (!passwordRegex.test(password as string)) {
-        throw new Error("Please provide a valid password");
+        alert("Please provide a valid password");
+        // throw new Error("Please provide a valid password");
       }
+
       // call login action
-      loginAction();
-      return redirect("/dashboard");
+      //   loginAction();
+      // appwrite api endpoint
+      toast.success("Login successful");
+      return redirect("/");
     } catch (error) {
-      toast.error(error.message);
-      throw new Error(error.message);
+      toast.error((error as Error).message);
+      //   throw new Error((error as Error).message);
     }
   }
-  if (actionType === "register") {
-    try {
-      const email = formData.get("email");
-      const password = formData.get("password");
+  //   if (actionType === "register") {
+  //     try {
+  //       const email = formData.get("email");
+  //       const password = formData.get("password");
 
-      if (email === "" || password === "") {
-        throw new Error("Please provide a valid input");
-      }
+  //       if (email === "" || password === "") {
+  //         throw new Error("Please provide a valid input");
+  //       }
 
-      if (!emailRegex.test(email as string)) {
-        throw new Error("Please provide a valid email");
-      }
+  //       if (!emailRegex.test(email as string)) {
+  //         throw new Error("Please provide a valid email");
+  //       }
 
-      if (!passwordRegex.test(password as string)) {
-        throw new Error("Please provide a valid password");
-      }
-      // call register action
-      registerAction();
-      return redirect("/login");
-    } catch (error) {
-      toast.error(error.message);
-      throw new Error(error.message);
-    }
-  }
+  //       if (!passwordRegex.test(password as string)) {
+  //         throw new Error("Please provide a valid password");
+  //       }
+  //       // call register action
+  //       //   registerAction();
+  //       // appwrite api endpoint
+  //       toast.success("Register successful");
+  //       return redirect("/login");
+  //     } catch (error) {
+  //       toast.error((error as Error).message);
+  //       throw new Error((error as Error).message);
+  //     }
+  //   }
 
-  if (actionType === "logout") {
-    try {
-      // call logout action
-      logoutAction();
-      return redirect("/login");
-    } catch (error) {
-      toast.error(error.message);
-      throw new Error(error.message);
-    }
-  }
+  //   if (actionType === "logout") {
+  //     try {
+  //       // call logout action
+  //       //   logoutAction();
+  //       return redirect("/login");
+  //     } catch (error) {
+  //       toast.error((error as Error).message);
+  //       throw new Error((error as Error).message);
+  //     }
+  //   }
 };
