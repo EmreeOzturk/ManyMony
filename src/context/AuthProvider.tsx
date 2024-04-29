7aaf7819f82ef96235fe8d1bd4f302818dec6512
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-29 06:27:08 +0300

    Refactor AuthProvider.tsx to include registration functionality and improve error handling

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,47 +1,10 @@
 import { createContext, useContext, useState } from "react";
-import { emailRegex, passwordRegex } from "../helper/consts";
+import { emailRegex, passwordRegex, userNameRexex } from "../helper/consts";
 import { account } from "../lib/appwrite";
 import { toast } from "react-toastify";
 import { redirect } from "react-router-dom";
 import type { Models } from "appwrite";
-// export interface defaultAuthContext {
-//     user: Models.Session | null;
-//     setUser?: (Models.Session | null) => void;
-//     loginAction: (email: string, password: string) => void;
-//     logoutAction?: () => void;
-// };
-
-// const defaultAuthContext: defaultAuthContext = {
-//     user: null,
-//     loginAction: async (email: string, password: string) => {
-//         try {
-//             if (email === "" || password === "") {
-//                 throw new Error("Please provide a valid input");
-//             }
-
-//             if (!emailRegex.test(email as string)) {
-//                 throw new Error("Please provide a valid email");
-//             }
-
-//             if (!passwordRegex.test(password as string)) {
-//                 throw new Error("Please provide a valid password");
-//             }
-
-//             const result: Models.Session = await account.createEmailPasswordSession(
-//                 email as string,
-//                 password as string
-//             );
-
-
-
-//             toast.success("Login successful");
-//             return redirect("/");
-//         } catch (error) {
-//             toast.error((error as Error).message);
-//             throw new Error((error as Error).message);
-//         }
-//     },
-// }
+import { ID } from "appwrite";
 
 const AuthContext = createContext({})
 
@@ -68,7 +31,6 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
                 email as string,
                 password as string
             );
-            console.log("denedikkk")
             setUser(result);
             toast.success("Login successful");
             return redirect("/");
@@ -78,11 +40,43 @@ const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         }
     }
 
-    const registerAction = async (email: string, password: string, name: string) => { }
+    const registerAction = async (email: string, password: string, confPassword: string, name: string) => {
+        try {
+            if (email === "" || password === "" || confPassword === "" || name === "") {
+                throw new Error("Please provide a valid input");
+            }
+
+            if (!emailRegex.test(email as string)) {
+                throw new Error("Please provide a valid email");
+            }
+
+            if (!passwordRegex.test(password as string)) {
+                throw new Error("Please provide a valid password");
+            }
+
+            if (password !== confPassword) {
+                throw new Error("Passwords do not match");
+            }
+
+            if (!userNameRexex.test(name as string)) {
+                throw new Error("Please provide a valid name");
+            }
+
+            console.log("hoop vurda")
+            // const result = await account.create(ID.unique(), email as string, password as string, name as string);
+            // console.log(result)
+            toast.success("Register successful");
+            return redirect("/login");
+        } catch (error) {
+            toast.error((error as Error).message);
+            throw new Error((error as Error).message);
+        }
+    }
 
     const store = {
         user,
-        loginAction
+        loginAction,
+        registerAction,
     }
 
     return (
