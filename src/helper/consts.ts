bd6c672f50a0274c1522abb62b235b63a7bad771
Emre Ozturk <emreeozturk42@gmail.com>
2024-04-29 06:27:11 +0300

    Add username validation to consts.ts

--- src/helper/consts.ts
+++ src/helper/consts.ts
@@ -4,5 +4,7 @@
 // 3. password must contain at least one number
 // 4. password must contain at least one uppercase letter
 // 5. password must contain at least one lowercase letter
+// 6. username must be between 3 and 20 characters long
 export const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 export const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+export const userNameRexex = /^[a-zA-Z0-9._-]{3,20}$/;
