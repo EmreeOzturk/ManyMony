6855d9db8d294377359371217466b8bff856ad8c
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-01 19:55:06 +0300

    Refactor Header component to include logo and authentication functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,14 @@
 // import logomark from '../assets/logomark.svg'
-import { NavLink } from 'react-router-dom'
-// import Trash from '../assets/icons/Trash'
 import { useAuth } from '../hooks/useAuth'
 import { Models } from 'appwrite';
-import WalletIcon from './icons/WalletIcon';
+import Logo from './Logo';
+import AuthButtons from './AuthButtons';
 const Header = (
-    // { userName }: { userName: string }
 ) => {
     const auth = useAuth();
     return (
         <header className='bg-transparent backdrop-blur-md z-10 text-neutral-400 max-w-7xl fixed left-0 right-0 mt-4 rounded-xl shadow-lg mx-auto flex justify-between items-center h-16 px-12'>
-            <div>
-                <NavLink to="/expenses" className="flex items-center justify-center gap-3 text-2xl tracking-widest font-mono
-                    bg-gradient-to-r from-violet-500 to-blue-700 text-transparent bg-clip-text logoAnimation
-                ">
-                    <WalletIcon className="logoAnimation" />
-                    <span>
-                        ManyMony
-                    </span>
-                </NavLink>
-            </div>
+            <Logo />
             {
                 (auth as {
                     user: Models.Session
@@ -34,14 +23,7 @@ const Header = (
                         Logout
                     </button>
                 ) : (
-                    <nav className='flex items-center justify-center gap-4 text-2xl font-mono'>
-                        <NavLink to="/login" aria-label='login link'>
-                            Login
-                        </NavLink>
-                        <NavLink to="/register" aria-label='register link'>
-                            Register
-                        </NavLink>
-                    </nav>
+                    <AuthButtons />
                 )
             }
         </header>
@@ -49,37 +31,3 @@ const Header = (
 }
 
 export default Header
-
-
-{/* <nav>
-<NavLink to="/" aria-label='Go to home'>
-    <img src={logomark} alt="logomark" />
-    <span>
-        Many Mony
-    </span>
-</NavLink>
-{
-    userName ? (
-        <Form action='/logout' method='POST'
-            onSubmit={(event) => {
-                if (!confirm('Are you sure you want to logout?')) {
-                    event.preventDefault()
-                }
-            }
-            }>
-            <button
-                type='submit'
-                aria-label='Logout'
-                className='btn btn--warning'
-            >
-                <Trash />
-                Logout
-            </button>
-        </Form>
-    ) : (
-        <div >
-
-        </div>
-    )
-}
-</nav > */}
