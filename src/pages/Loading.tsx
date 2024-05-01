82f3c003c1e6fafddb858c3a23549cbea6bfdb00
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-01 21:35:02 +0300

    Update Loading component to use wallet icon in Loading.tsx

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -19,10 +19,11 @@ const Loading = () => {
                         stroke='currentColor'
                         strokeWidth='4'
                     ></circle>
+                    {/* wallet  */}
                     <path
                         className='opacity-75'
                         fill='currentColor'
-                        d='M4 12a8 8 0 018-8V4a10 10 0 00-10 10h2zm2 0a6 6 0 016-6V6a8 8 0 00-8 8h2zm2 0a4 4 0 014-4V4a6 6 0 00-6 6h2zm2 0a2 2 0 012-2V2a4 4 0 00-4 4h2z'
+                        d='M12 14c-2.21 0-4-1.79-4-4s1.79-4 4-4s4 1.79 4 4s-1.79 4-4 4zm0-6c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2z'
                     ></path>
                 </svg>
                 <h1 className='text-2xl font-bold text-neutral-100 mt-4'>
