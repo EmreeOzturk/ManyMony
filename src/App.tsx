1b5b1e228cbe40afe14cbf4239f1f75979b23976
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-01 21:21:54 +0300

    Fix formatting and remove unnecessary code in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import { Outlet } from "react-router-dom"
-// import wave from "./assets/wave.svg"
 import Header from "./components/Header";
 function App() {
-  // const { userName } = useLoaderData() as { userName: string };
   return (
-    <
-      // className="layout"
-      >
+    <>
       <Header />
       <main>
         <Outlet />
       </main>
-      <footer>
-        {/* <img src={wave} alt="wave" /> */}
-      </footer>
     </>
   )
 }
