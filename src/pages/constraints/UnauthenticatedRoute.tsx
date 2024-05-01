62d505e367c5b8b6cb2918aa88827f950fd53165
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-01 20:54:04 +0300

    Add UnauthenticatedRoute component for handling unauthenticated routes

---
+++ src/pages/constraints/UnauthenticatedRoute.tsx
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from '../../hooks/useAuth'
+import { Models } from "appwrite";
+const UnauthenticatedRoute = () => {
+    const auth = useAuth() as { user: Models.Session | null };
+    if (auth?.user) return <Navigate to="/" />;
+    return <Outlet />;
+}
+
+export default UnauthenticatedRoute
