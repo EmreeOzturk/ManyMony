02d01c413782a38b955ee2c9eca9381ff8a92e81
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-01 20:54:17 +0300

    Update Logo component styling and colors

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import WalletIcon from "./icons/WalletIcon"
 const Logo = () => {
     return (
         <div>
-            <NavLink to="/expenses" className="flex items-center justify-center gap-3 text-2xl  font-mono font-bold
+            <NavLink to="/" className="flex items-center justify-center gap-3 text-2xl  font-mono font-bold
                     bg-gradient-to-r from-cyan-700  to-violet-800 text-transparent bg-clip-text 
                 ">
                 <WalletIcon />
