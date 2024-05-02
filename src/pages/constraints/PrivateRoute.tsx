98d609a167722eb6a41dc65c0658502632f856fe
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-03 02:21:44 +0300

    Add navigation and layout to PrivateRoute component

--- src/pages/constraints/PrivateRoute.tsx
+++ src/pages/constraints/PrivateRoute.tsx
@@ -4,7 +4,18 @@ import { Models } from "appwrite";
 const PrivateRoute = () => {
     const auth = useAuth() as { user: Models.Session | null };
     if (!auth?.user) return <Navigate to="/login" />;
-    return <Outlet />;
+    return (<main className="flex ">
+        <nav className=" flex flex-col items-center justify-center h-screen w-60 bg-slate-500 ">
+            <ul className="flex flex-col p-4">
+                <li className="p-2">Dashboard</li>
+                <li className="p-2">Budgets</li>
+                <li className="p-2">Expenses</li>
+            </ul>
+        </nav>
+        <section className="bg-sky-950 flex-1">
+            <Outlet />
+        </section>
+    </main>)
 }
 
 export default PrivateRoute
