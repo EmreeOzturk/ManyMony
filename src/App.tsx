774c7e2d5c7d41d4bea612b3835eb38efe1fcb14
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-02 09:56:24 +0300

    Refactor App.tsx to add AuthProvider and conditionally render Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom"
+import AuthProvider from "./context/AuthProvider";
 import Header from "./components/Header";
+import { useLocation } from "react-router-dom";
 function App() {
+  const location = useLocation();
   return (
-    <>
-      <Header />
+    <AuthProvider>
+      {
+        location.pathname == "/" && <Header />
+      }
       <main>
         <Outlet />
       </main>
-    </>
+    </AuthProvider>
   )
 }
 
