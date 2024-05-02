775622e3b26d4cf96e947f30b9127b7ff243f6e1
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-02 09:14:07 +0300

    Add useNavigate hook to Logo component for navigation

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,17 +1,27 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import WalletIcon from "./icons/WalletIcon"
 
 const Logo = () => {
+    const navigate = useNavigate()
     return (
         <div>
             <NavLink to="/" className="flex items-center justify-center gap-3 text-2xl  font-mono font-bold
                     bg-gradient-to-r from-cyan-700  to-violet-800 text-transparent bg-clip-text 
                 ">
                 <WalletIcon />
-                <span>
+                <span >
                     ManyMony
                 </span>
             </NavLink>
+
+            <button
+                onClick={() => {
+                    navigate('/dashboard')
+                }}
+                className="bg-gradient-to-r from-cyan-700  to-violet-800 text-transparent bg-clip-text text-lg font-mono"
+            >
+                Test
+            </button>
         </div>
     )
 }
