f7dcacfff5cf78612b2504ed24ffa28263950d0f
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-02 09:56:28 +0300

    Refactor router configuration in main.tsx to remove unused code and fix path for dashboard page

--- src/main.tsx
+++ src/main.tsx
@@ -1,97 +1,95 @@
 import './index.css'
 import ReactDOM from 'react-dom/client'
-// import LangingPage from './pages/LangingPage.tsx'
-// import { expensesLoader } from './loaders/expensesLoader.ts'
-// import { expensesAction } from './actions/expensesAction.ts'
-// import { mainLoader } from './loaders/mainLoader.ts'
-// import { dashboardLoader } from './loaders/DashboardLoader.ts'
+import LangingPage from './pages/LangingPage.tsx'
+import { expensesLoader } from './loaders/expensesLoader.ts'
+import { expensesAction } from './actions/expensesAction.ts'
+import { mainLoader } from './loaders/mainLoader.ts'
+import { dashboardLoader } from './loaders/DashboardLoader.ts'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
-// import {
-//   createBrowserRouter,
-//   // RouterProvider,
-// } from "react-router-dom";
-// import Error from './pages/Error.tsx'
-// import App from './App.tsx'
-// import AllExpenses from './pages/dashboard/AllExpenses.tsx'
-// import { logoutAction } from './actions/logoutAction.ts'
-// import { dashboardAction } from './actions/dashboardAction.ts'
-// import BudgetDetail from './pages/dashboard/BudgetDetail.tsx'
-// import { budgetDetailLoader } from './loaders/budgetDetailLoader.ts'
-// import { budgetDetailAction } from './actions/budgetDetailAction.ts'
-// import LoginPage from './pages/auth/LoginPage.tsx'
-// import RegisterPage from './pages/auth/RegisterPage.tsx'
-// import PrivateRoute from './pages/constraints/PrivateRoute.tsx'
-// import UnauthenticatedRoute from './pages/constraints/UnauthenticatedRoute.tsx'
-import AuthProvider from './context/AuthProvider.tsx'
-import RouterProviderr from './context/RouterContext.tsx'
-// import Dashboard from './pages/dashboard/Dashboard.tsx'
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//     loader: mainLoader,
-//     errorElement: <Error />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <LangingPage />,
-//         loader: dashboardLoader,
-//         errorElement: <Error />,
-//         action: dashboardAction
-//       },
-//       {
-//         element: <PrivateRoute />,
-//         children: [
-//           {
-//             path: "/expenses",
-//             element: <AllExpenses />,
-//             loader: expensesLoader,
-//             action: expensesAction,
-//             errorElement: <Error />,
-//           },
-//           {
-//             path: "dashboard",
-//             element: <div>dashboard page</div>,
-//             // loader: dashboardLoader,
-//             // action: dashboardAction,
-//             errorElement: <Error />,
-//           },
-//           {
-//             path: "/budgets/:budgetId",
-//             element: <BudgetDetail />,
-//             errorElement: <Error />,
-//             loader: budgetDetailLoader,
-//             action: budgetDetailAction
-//           }
-//         ]
-//       },
-//       {
-//         path: "/logout",
-//         action: logoutAction,
-//       },
-//     ],
-//   },
-//   {
-//     element: <UnauthenticatedRoute />,
-//     children: [
-//       {
-//         path: "/login",
-//         element: <LoginPage />,
-//       },
-//       {
-//         path: "/register",
-//         element: <RegisterPage />,
-//       }
-//     ]
-//   }
-// ]);
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Error from './pages/Error.tsx'
+import App from './App.tsx'
+import AllExpenses from './pages/dashboard/AllExpenses.tsx'
+import { logoutAction } from './actions/logoutAction.ts'
+import { dashboardAction } from './actions/dashboardAction.ts'
+import BudgetDetail from './pages/dashboard/BudgetDetail.tsx'
+import { budgetDetailLoader } from './loaders/budgetDetailLoader.ts'
+import { budgetDetailAction } from './actions/budgetDetailAction.ts'
+import LoginPage from './pages/auth/LoginPage.tsx'
+import RegisterPage from './pages/auth/RegisterPage.tsx'
+import PrivateRoute from './pages/constraints/PrivateRoute.tsx'
+import UnauthenticatedRoute from './pages/constraints/UnauthenticatedRoute.tsx'
+import Dashboard from './pages/dashboard/Dashboard.tsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <LangingPage />,
+        loader: dashboardLoader,
+        errorElement: <Error />,
+        action: dashboardAction
+      },
+      {
+        element: <UnauthenticatedRoute />,
+        children: [
+          {
+            path: "/login",
+            element: <LoginPage />,
+          },
+          {
+            path: "/register",
+            element: <RegisterPage />,
+          }
+        ]
+      },
+      {
+        element: <PrivateRoute />,
+        children: [
+          {
+            path: "/expenses",
+            element: <AllExpenses />,
+            loader: expensesLoader,
+            action: expensesAction,
+            errorElement: <Error />,
+          },
+
+          {
+            path: "/budgets/:budgetId",
+            element: <BudgetDetail />,
+            errorElement: <Error />,
+            loader: budgetDetailLoader,
+            action: budgetDetailAction
+          }
+        ]
+      },
+      {
+        path: "/logout",
+        action: logoutAction,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    loader: dashboardLoader,
+    action: dashboardAction,
+    errorElement: <Error />,
+  },
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProviderr>
-    <AuthProvider>
-      {/* <RouterProvider router={router} /> */}
-      <ToastContainer />
-    </AuthProvider>
-  </RouterProviderr>
+  <>
+    <RouterProvider router={router} />
+    <ToastContainer />
+  </>
 )
