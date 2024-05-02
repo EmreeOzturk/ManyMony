d41ed2be570d9b4480b904a85bac4ec612021fab
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-03 02:14:05 +0300

    removed unused files

--- src/loaders/mainLoader.ts
+++
@@ -1,9 +0,0 @@
-import { fetchDataFromLocalStorage } from "../helper";
-
-export const mainLoader = () => {
-  const data = fetchDataFromLocalStorage("userName");
-  if (data) {
-    return { userName: data as string };
-  }
-  return { userName: "" };
-};
