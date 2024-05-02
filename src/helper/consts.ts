2d8d8e9f1df19336304573534ce34a20cf46cb28
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-02 12:02:09 +0300

    Add phoneRegex constant for phone number validation in consts.ts

--- src/helper/consts.ts
+++ src/helper/consts.ts
@@ -8,3 +8,4 @@
 export const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 export const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
 export const userNameRexex = /^[a-zA-Z-].{6,20}$/;
+export const phoneRegex = /^[5]\d{9}$/;
