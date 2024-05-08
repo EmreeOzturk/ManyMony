b54734f0385431d0c1741f793df643d8a96e861e
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-08 12:36:54 +0300

    refactor: Update Tabs component to include tab reordering and hover effect

---
+++ src/components/ui/tabs.tsx
@@ -0,0 +1,140 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { cn } from "@/src/utils/cn";
+
+type Tab = {
+    title: string;
+    value: string;
+    content?: string | React.ReactNode;
+};
+
+export const Tabs = ({
+    tabs: propTabs,
+    containerClassName,
+    activeTabClassName,
+    tabClassName,
+    contentClassName,
+}: {
+    tabs: Tab[];
+    containerClassName?: string;
+    activeTabClassName?: string;
+    tabClassName?: string;
+    contentClassName?: string;
+}) => {
+    const [active, setActive] = useState<Tab>(propTabs[0]);
+    const [tabs, setTabs] = useState<Tab[]>(propTabs);
+
+    const moveSelectedTabToTop = (idx: number) => {
+        const newTabs = [...propTabs];
+        const selectedTab = newTabs.splice(idx, 1);
+        newTabs.unshift(selectedTab[0]);
+        setTabs(newTabs);
+        setActive(newTabs[0]);
+    };
+
+    const [hovering, setHovering] = useState(false);
+
+    return (
+        <>
+            <div
+                className={cn(
+                    "flex flex-row items-center justify-start [perspective:1000px] relative overflow-auto sm:overflow-visible no-visible-scrollbar max-w-full w-full",
+                    containerClassName
+                )}
+            >
+                {propTabs.map((tab, idx) => (
+                    <button
+                        key={tab.title}
+                        onClick={() => {
+                            moveSelectedTabToTop(idx);
+                        }}
+                        onMouseEnter={() => setHovering(true)}
+                        onMouseLeave={() => setHovering(false)}
+                        className={cn("relative px-4 py-2 rounded-full", tabClassName)}
+                        style={{
+                            transformStyle: "preserve-3d",
+                        }}
+                    >
+                        {active.value === tab.value && (
+                            <motion.div
+                                layoutId="clickedbutton"
+                                transition={{ type: "spring", bounce: 0.3, duration: 0.6 }}
+                                className={cn(
+                                    "absolute inset-0  bg-zinc-800 rounded-full ",
+                                    activeTabClassName
+                                )}
+                            />
+                        )}
+
+                        <span className="relative block text-white dark:text-white">
+                            {tab.title}
+                        </span>
+                    </button>
+                ))}
+
+                <div className="absolute right-0 flex gap-2 items-center" >
+                    <button
+                        className={cn("relative px-4 py-2 rounded-md bg-transparent border border-zinc-500")}
+                    >
+                        <span className="relative block text-white text-2xl">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M304 336v40a40 40 0 0 1-40 40H104a40 40 0 0 1-40-40V136a40 40 0 0 1 40-40h152c22.09 0 48 17.91 48 40v40m64 160 80-80-80-80m-192 80h256"></path></svg>
+                        </span>
+                    </button>
+
+                    <button
+                        className={cn("relative px-4 py-2 rounded-md bg-transparent border border-zinc-500")}
+                    >
+                        <span className="relative block text-white text-2xl ">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><g id="User"><g><path d="M17.438,21.937H6.562a2.5,2.5,0,0,1-2.5-2.5V18.61c0-3.969,3.561-7.2,7.938-7.2s7.938,3.229,7.938,7.2v.827A2.5,2.5,0,0,1,17.438,21.937ZM12,12.412c-3.826,0-6.938,2.78-6.938,6.2v.827a1.5,1.5,0,0,0,1.5,1.5H17.438a1.5,1.5,0,0,0,1.5-1.5V18.61C18.938,15.192,15.826,12.412,12,12.412Z"></path><path d="M12,9.911a3.924,3.924,0,1,1,3.923-3.924A3.927,3.927,0,0,1,12,9.911Zm0-6.847a2.924,2.924,0,1,0,2.923,2.923A2.926,2.926,0,0,0,12,3.064Z"></path></g></g></svg>
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <FadeInDiv
+                tabs={tabs}
+                active={active}
+                key={active.value}
+                hovering={hovering}
+                className={cn("mt-32", contentClassName)}
+            />
+        </>
+    );
+};
+
+export const FadeInDiv = ({
+    className,
+    tabs,
+    hovering,
+}: {
+    className?: string;
+    key?: string;
+    tabs: Tab[];
+    active: Tab;
+    hovering?: boolean;
+}) => {
+    const isActive = (tab: Tab) => {
+        return tab.value === tabs[0].value;
+    };
+    return (
+        <div className="relative w-full h-full">
+            {tabs.map((tab, idx) => (
+                <motion.div
+                    key={tab.value}
+                    layoutId={tab.value}
+                    style={{
+                        scale: 1 - idx * 0.1,
+                        top: hovering ? idx * -50 : 0,
+                        zIndex: -idx,
+                        opacity: idx < 3 ? 1 - idx * 0.1 : 0,
+                    }}
+                    animate={{
+                        y: isActive(tab) ? [0, 40, 0] : 0,
+                    }}
+                    className={cn("w-full h-full absolute top-0 left-0", className)}
+                >
+                    {tab.content}
+                </motion.div>
+            ))}
+        </div>
+    );
+};
