b5c0341eeb2847aeb6861831b962ffcda1f29223
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-08 14:32:07 +0300

    refactor: Update Logo component to use NavLink for better navigation

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -3,16 +3,14 @@ import WalletIcon from "../icons/WalletIcon"
 
 const Logo = () => {
     return (
-        <div>
-            <NavLink to="/" className="flex items-center justify-center gap-3 text-lg md:text-2xl  font-mono font-bold
+        <NavLink to="/" className="flex items-center justify-center gap-3 text-lg md:text-2xl  font-mono font-bold
                     bg-gradient-to-r from-cyan-700  to-violet-800 text-transparent bg-clip-text 
                 ">
-                <WalletIcon />
-                <span >
-                    ManyMony
-                </span>
-            </NavLink>
-        </div>
+            <WalletIcon />
+            <span >
+                ManyMony
+            </span>
+        </NavLink>
     )
 }
 
