5868361b8df6ec53252bff1ec84029ef09d757d1
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-08 12:37:00 +0300

    refactor: Update Dashboard component to include Tabs component

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,10 @@
+import { Tabs } from "@/src/components/ui/tabs"
+import { tabs } from "../constraints/PrivateRoute"
 
 const Dashboard = () => {
     return (
-        <div>
-            its dashnoard
+        <div className="h-3/4 px-10 mt-10 [perspective:1000px] relative flex flex-col mx-auto w-full  items-start justify-start ">
+            <Tabs tabs={tabs} />
         </div>
     )
 }
