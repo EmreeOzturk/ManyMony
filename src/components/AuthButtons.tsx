8ceed0fa270239a71fb7bad73f9be7f7be4fca0c
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-08 09:43:05 +0300

    chore: Update AuthButtons component styling for login and register buttons

--- src/components/AuthButtons.tsx
+++ src/components/AuthButtons.tsx
@@ -4,7 +4,7 @@ const AuthButtons = () => {
     return (
         <nav className='flex items-center justify-center gap-4 font-mono'>
             <NavLink to="/login" aria-label='login link'>
-                <button className="relative inline-flex h-12  w-24 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                <button className="relative inline-flex h-10  w-20 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                     <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                     <span className="inline-flex  h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-md font-medium text-white backdrop-blur-3xl">
                         Login
@@ -12,7 +12,7 @@ const AuthButtons = () => {
                 </button>
             </NavLink>
             <NavLink to="/register" aria-label='register link'>
-                <button className="relative inline-flex h-12 w-28 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+                <button className="relative inline-flex h-10 w-24 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
                     <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                     <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-md font-medium text-white backdrop-blur-3xl">
                         Register
