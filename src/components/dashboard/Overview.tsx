4de1f1721f03dd1e2875fcb6c818a0a40453d5ac
emreact <20180808030@ogr.akdeniz.edu.tr>
2024-05-11 10:20:18 +0300

    refactor: Update Overview component to display total income, expense, and budget

--- src/components/dashboard/Overview.tsx
+++ src/components/dashboard/Overview.tsx
@@ -1,8 +1,23 @@
-
 const Overview = () => {
-    return (
-        <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-zinc-900">Overview</div>
-    )
-}
+  return (
+    <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-zinc-900">
+      <div className="flex gap-2">
+        <div className="w-60 h-32 border rounded-lg text-2xl">
+          Total Income : 500
+        </div>
+        <div className="w-60 h-32 border rounded-lg text-2xl">
+          Total Expense : 800
+        </div>
+        <div className="w-60 h-32 border rounded-lg text-2xl">
+          Total Budget : 1200
+        </div>
 
-export default Overview
+        <div>
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Overview;
