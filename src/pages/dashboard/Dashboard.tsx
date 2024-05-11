84b47f68590261c6dbd2b1992a6126c9f99b114f
emreact <20180808030@ogr.akdeniz.edu.tr>
2024-05-11 12:49:18 +0300

    refactor: Update Dashboard component to improve code formatting and consistency

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,14 +1,14 @@
-import { Tabs } from "@/src/components/ui/tabs"
-import useTabs from "@/src/hooks/useTabs"
+import { Tabs } from "@/src/components/ui/tabs";
+import useTabs from "@/src/hooks/useTabs";
 
 const Dashboard = () => {
-    const tabs = useTabs()
+  const tabs = useTabs();
 
-    return (
-        <div className="h-3/4 px-10 mt-10 [perspective:1000px] relative flex flex-col mx-auto w-full  items-start justify-start ">
-            <Tabs tabs={tabs} />
-        </div>
-    )
-}
+  return (
+    <div className="h-[82%] px-10 mt-10 [perspective:1000px] relative flex flex-col mx-auto w-full  items-start justify-start ">
+      <Tabs tabs={tabs} />
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
