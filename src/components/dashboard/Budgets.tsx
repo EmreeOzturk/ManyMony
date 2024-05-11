c94864b6c5b7c80ec5800480d1fadf13095f47c6
emreact <20180808030@ogr.akdeniz.edu.tr>
2024-05-11 12:49:14 +0300

    refactor: Update Analyze, Budgets, and Expenses components to improve readability

--- src/components/dashboard/Budgets.tsx
+++ src/components/dashboard/Budgets.tsx
@@ -1,7 +1,8 @@
 
 const Budgets = () => {
   return (
-    <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-zinc-900">Budgets</div>
+    <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-zinc-900">
+      Budgets</div>
   )
 }
 
