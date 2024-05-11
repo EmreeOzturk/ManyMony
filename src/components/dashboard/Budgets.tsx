e9c9a403c3f90da6392d31a0889390a60b5e7317
emreact <20180808030@ogr.akdeniz.edu.tr>
2024-05-11 16:26:27 +0300

    refactor: Update Budgets component to use consistent formatting and add missing semicolon

--- src/components/dashboard/Budgets.tsx
+++ src/components/dashboard/Budgets.tsx
@@ -2,8 +2,9 @@
 const Budgets = () => {
   return (
     <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-zinc-900">
-      Budgets</div>
-  )
-}
+     Budgets
+    </div>
+  );
+};
 
-export default Budgets
+export default Budgets;
