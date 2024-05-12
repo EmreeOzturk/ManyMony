a2f6e0ff319e2038f6abe2e524276b74c11b3a85
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-12 09:01:44 +0300

    refactor: Update Budgets component to display dynamic budget cards with different categories and amounts

--- src/components/dashboard/budgets/Budgets.tsx
+++ src/components/dashboard/budgets/Budgets.tsx
@@ -2,11 +2,32 @@ import BudgetCard from "./BudgetCard";
 
 const Budgets = () => {
   return (
-    <div className="w-full gap-2 overflow-hidden flex items-center   flex-wrap justify-center relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-zinc-900">
-      <BudgetCard />
-      <BudgetCard />
-      <BudgetCard />
-      <BudgetCard />
+    <div className="w-full gap-4 overflow-y-auto px-16 overflow-hidden flex items-start flex-wrap justify-center relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-zinc-900">
+      <BudgetCard
+        name="Food"
+        amount={200}
+        percentage={50}
+        categories={["Groceries", "Eating Out"]}
+      />
+      <BudgetCard
+        name="Transport"
+        amount={100}
+        percentage={25}
+        categories={["Fuel", "Public Transport"]}
+      />
+      <BudgetCard
+        name="Entertainment"
+        amount={50}
+        percentage={12.5}
+        categories={["Movies", "Games"]}
+      />
+      <BudgetCard
+        name="Shopping"
+        amount={100}
+        percentage={25}
+        categories={["Clothes", "Electronics"]}
+      />
+   
     </div>
   );
 };
