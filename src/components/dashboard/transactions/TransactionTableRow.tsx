6f32b636f18ff7341d0cc933796e4e1d06c59d2d
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-12 12:26:41 +0300

    refactor: Add TransactionTableRow component for displaying transaction data in a table

---
+++ src/components/dashboard/transactions/TransactionTableRow.tsx
@@ -0,0 +1,41 @@
+import { Ellipsis } from "lucide-react";
+import { TableCell } from "../../ui/table";
+import { motion } from "framer-motion";
+
+const TransactionTableRow = ({
+  date,
+  name,
+  category,
+  amount,
+  index,
+}: {
+  date: string;
+  name: string;
+  category: string;
+  amount: number;
+  index: number;
+}) => {
+  return (
+    <motion.tr
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: index * 0.1 }}
+      className="w-full hover:bg-indigo-950/60" >
+      <TableCell className="font-medium">
+        <span>{date}</span>
+      </TableCell>
+      <TableCell>
+        <span>{name}</span>
+      </TableCell>
+      <TableCell className="text-right">
+        <span>{category}</span>
+      </TableCell>
+      <TableCell className="text-right">${amount}</TableCell>
+      <TableCell className="flex justify-end pr-6">
+        <Ellipsis />
+      </TableCell>
+    </motion.tr>
+  );
+};
+
+export default TransactionTableRow;
