d6bdada3beeb44e4413a0490ff540a212fd57811
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-12 08:50:17 +0300

    feat: Add RecentExpenseCard and RecentExpenses components for displaying recent expense information on the dashboard

---
+++ src/components/dashboard/overview/RecentExpenseCard.tsx
@@ -0,0 +1,29 @@
+const RecentExpenseCard = ({
+  title,
+  value,
+  date,
+}: {
+  title: string;
+  value: number;
+  date: string;
+}) => {
+  return (
+    <div className="bg-zinc-800/70 flex flex-col items-start py-2 px-4  justify-between w-full h-16 rounded-md">
+      <div className="w-full flex justify-between items-center">
+        <h4 className="text-lg">{title}</h4>
+        <p className="text-lg">${value}</p>
+      </div>
+      <div>
+        <p className="text-sm text-zinc-600">
+          {new Date(date).toLocaleString("en-US", {
+            month: "short",
+            day: "numeric",
+            year: "numeric",
+          })}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RecentExpenseCard;
