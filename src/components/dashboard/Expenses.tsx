718dfb01951e87acdfcea85b72fcaa89383a2dfe
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-12 09:24:18 +0300

    refactor: Remove Expenses component from dashboard

--- src/components/dashboard/Expenses.tsx
+++
@@ -1,9 +0,0 @@
-
-const Expenses = () => {
-  return (
-    <div className="w-full overflow-hidden relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-white bg-zinc-900">
-      Expenses</div>
-  )
-}
-
-export default Expenses
