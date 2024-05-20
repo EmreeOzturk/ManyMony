246b6555b8bd80340110e032a9b5556b16c2423d
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-20 19:19:37 +0300

    refactor: Update Overview component to use useOverviewData hook for fetching data

--- src/components/dashboard/overview/Overview.tsx
+++ src/components/dashboard/overview/Overview.tsx
@@ -1,15 +1,17 @@
-// primary bg color = bg-indigo-950
-
 import { TrendingUp, TrendingDown } from "lucide-react";
 import OverviewCard from "./OverviewCard";
 import RecentExpenses from "./RecentExpenses";
 import RecentBudgets from "./RecentBudgets";
 import { BarChart } from "@tremor/react";
 import { chartdata2 } from "../analytic/Analytics";
-
+import { useOverviewData } from "@/src/hooks/useOverviewData";
 const dataFormatter = (number: number) =>
   Intl.NumberFormat("us").format(number).toString();
 const Overview = () => {
+  const { totalBudget, totalExpense, recentBudgets, recentExpenses } =
+    useOverviewData();
+
+  console.log(totalBudget, totalExpense, recentBudgets, recentExpenses);
   return (
     <div className="flex gap-2 flex-col justify-between items-start w-full overflow-hidden relative h-full rounded-2xl p-4 text-xl md:text-4xl font-bold text-zinc-300 bg-zinc-900">
       <div className="flex w-full gap-2 flex-1">
@@ -17,12 +19,12 @@ const Overview = () => {
           <div className="flex gap-2">
             <OverviewCard
               title="Total Budget"
-              value={1200}
+              value={totalBudget}
               Icon={<TrendingUp className="text-green-400" />}
             />
             <OverviewCard
               title="Total Expense"
-              value={800}
+              value={totalExpense}
               Icon={<TrendingDown className="text-red-400" />}
             />
           </div>
@@ -38,9 +40,9 @@ const Overview = () => {
             />
           </div>
         </div>
-        <RecentExpenses />
+        <RecentExpenses data={recentExpenses} />
       </div>
-      <RecentBudgets />
+      <RecentBudgets data={recentBudgets} />
     </div>
   );
 };
