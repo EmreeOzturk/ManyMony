c18938cd1f32ad4be60f4afa7e965ccc1e0a1338
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-20 19:51:51 +0300

    refactor: Update RecentBudgets component to use updated data structure and dynamically render RecentBudgetCard

--- src/components/dashboard/overview/RecentBudgets.tsx
+++ src/components/dashboard/overview/RecentBudgets.tsx
@@ -1,15 +1,23 @@
-import { Banana, Drama, Plane } from "lucide-react";
+import { Drama } from "lucide-react";
 import RecentBudgetCard from "./RecentBudgetCard";
-import { Models } from "appwrite";
 
-const RecentBudgets = ({ data }: { data: Models.Document[] }) => {
+const RecentBudgets = ({
+  data,
+}: {
+  data: {
+    name: string;
+    limit: number;
+    usage: number;
+    id: string;
+  }[];
+}) => {
   return (
     <div className="border flex flex-col p-4 bg-indigo-950/30 overflow-y-hidden items-start gap-2 justify-start border-indigo-500/40 w-full  rounded-md ">
       <h3 className="text-2xl">Recent Budgets</h3>
       {data.map((budget) => {
         return (
           <RecentBudgetCard
-            key={budget.$id}
+            key={budget.id}
             title={budget.name}
             percentage={(budget.usage / budget.limit) * 100}
             Icon={<Drama />}
