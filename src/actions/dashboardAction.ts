3c7fdeeed1a33bed6ecd24dccc0a56cd6505f4eb
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-20 19:19:24 +0300

    refactor: Remove unused code and improve error handling in dashboardAction.ts

--- src/actions/dashboardAction.ts
+++ src/actions/dashboardAction.ts
@@ -1,6 +1,6 @@
 import { toast } from "react-toastify";
 import type { ActionFunction } from "react-router-dom";
-import { saveDataToLocalStorage, createExpense } from "../helper";
+import { saveDataToLocalStorage } from "../helper";
 import { databases } from "../lib/appwrite";
 import { ID } from "appwrite";
 export const dashboardAction: ActionFunction = async ({
@@ -28,12 +28,7 @@ export const dashboardAction: ActionFunction = async ({
     const categpry = formData.get("budgetCategory");
     const userId = formData.get("userId");
     const errors = {} as Record<string, string>;
-    console.log({
-      budgetName,
-      budgetLimit,
-      categpry,
-      userId,
-    });
+
     if (!budgetName) {
       errors["budgetName"] = "Budget name is required";
       toast.error("Budget name is required", {
@@ -59,18 +54,18 @@ export const dashboardAction: ActionFunction = async ({
       return errors;
     }
     try {
-      // const result = await databases.createDocument(
-      //   "66343e800011dbbdd0f4",
-      //   "66343eb4001c491d89a7",
-      //   ID.unique(),
-      //   {
-      //     name: budgetName?.toString(),
-      //     limit: Number(budgetLimit),
-      //     category: categpry,
-      //     userId: userId?.toString(),
-      //   }
-      // );
-      // console.log(result);
+      const result = await databases.createDocument(
+        "66343e800011dbbdd0f4",
+        "66343eb4001c491d89a7",
+        ID.unique(),
+        {
+          name: budgetName?.toString(),
+          limit: Number(budgetLimit),
+          category: categpry,
+          userId: userId?.toString(),
+        }
+      );
+      console.log(result);
       toast.success("Budget added successfully", {
         autoClose: 2000,
         position: "bottom-right",
@@ -89,19 +84,71 @@ export const dashboardAction: ActionFunction = async ({
   }
 
   if (actionType === "createExpense") {
+    const budgetId = formData.get("budget");
+    const expenseAmount = formData.get("amount");
+    const userId = formData.get("userId");
+    const errors = {} as Record<string, string>;
+
+    if (!budgetId) {
+      errors["budget"] = "Budget is required";
+      toast.error("Budget is required", {
+        autoClose: 2000,
+        position: "bottom-right",
+      });
+    }
+
+    if (!expenseAmount) {
+      errors["amount"] = "Amount is required";
+      toast.error("Amount is required", {
+        autoClose: 2000,
+        position: "bottom-right",
+      });
+    }
+
+    if (Object.keys(errors).length > 0) {
+      return errors;
+    }
+
     try {
-      const budgetId = formData.get("newExpenseBudget");
-      const expenseName = formData.get("newExpense");
-      const expenseAmount = formData.get("newExpenseAmount");
-      await createExpense(
-        budgetId?.toString() || "",
-        expenseName?.toString() || "",
-        expenseAmount?.toString() || ""
+      const result = await databases.createDocument(
+        "66343e800011dbbdd0f4",
+        "66343ebc0025e1be1729",
+        ID.unique(),
+        {
+          budget: budgetId?.toString(),
+          amount: Number(expenseAmount),
+          userId: userId?.toString(),
+        }
       );
-      return toast.success(`Expense ${expenseName} added successfully`);
+      const oldUsage = await databases.getDocument(
+        "66343e800011dbbdd0f4",
+        "66343eb4001c491d89a7",
+        budgetId?.toString() as string
+      );
+
+      await databases.updateDocument(
+        "66343e800011dbbdd0f4",
+        "66343eb4001c491d89a7",
+        budgetId?.toString() as string,
+        {
+          usage: Number(oldUsage.usage) + Number(expenseAmount),
+        }
+      );
+      console.log(result);
+      toast.success("Expense added successfully", {
+        autoClose: 2000,
+        position: "bottom-right",
+      });
+      return {
+        message: "Expense added successfully",
+        status: "success",
+      };
     } catch (error) {
-      toast.error("Failed to add expense");
-      throw new Error("Failed to add expense");
+      console.log(error);
+      return {
+        message: "Failed to create expense",
+        status: "error",
+      };
     }
   }
 };
