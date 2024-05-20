32d9153fab88c60a3d5479cf4bbacd104d0ad0cd
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-20 14:47:27 +0300

    refactor: Update Budgets component styles for better responsiveness

--- src/components/dashboard/budgets/Budgets.tsx
+++ src/components/dashboard/budgets/Budgets.tsx
@@ -2,7 +2,7 @@ import BudgetCard from "./BudgetCard";
 
 const Budgets = () => {
   return (
-    <div className="w-full gap-4  overflow-y-auto px-20 mx-auto overflow-hidden flex items-center flex-wrap justify-center relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-zinc-300 bg-zinc-900">
+    <div className="w-full gap-4 overflow-y-auto px-10 mx-auto overflow-hidden flex items-center flex-wrap justify-center relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-zinc-300 bg-zinc-900">
       <BudgetCard
         name="Food"
         amount={200}
