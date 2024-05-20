5e983ba3afcd7c759317a95ef55814ae81544458
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-20 19:51:12 +0300

    refactor: Update Budgets component to use useLoaderData hook for fetching documents and render BudgetCard dynamically

--- src/components/dashboard/budgets/Budgets.tsx
+++ src/components/dashboard/budgets/Budgets.tsx
@@ -1,9 +1,24 @@
+import { Models } from "appwrite";
 import BudgetCard from "./BudgetCard";
-
+import { useLoaderData } from "react-router-dom";
 const Budgets = () => {
+  const { documents } = useLoaderData() as { documents: Models.Document[] };
+  console.log(documents);
   return (
     <div className="w-full gap-4 overflow-y-auto px-10 mx-auto overflow-hidden flex items-center flex-wrap justify-center relative h-full rounded-2xl p-10 text-xl md:text-4xl font-bold text-zinc-300 bg-zinc-900">
-      <BudgetCard
+      {documents.map((budget, index) => {
+        return (
+          <BudgetCard
+            key={budget.$id}
+            name={budget.name}
+            amount={budget.limit}
+            percentage={(budget.usage / budget.limit) * 100}
+            categories={budget.category}
+            index={index}
+          />
+        );
+      })}
+      {/* <BudgetCard
         name="Food"
         amount={200}
         percentage={50}
@@ -53,7 +68,7 @@ const Budgets = () => {
         percentage={12.5}
         categories={["Movies", "Games"]}
         index={7}
-      />
+      /> */}
     </div>
   );
 };
