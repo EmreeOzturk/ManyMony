1e15dd8f40a669bc5fa633af3ac7d738510a150d
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-20 14:47:44 +0300

    refactor: Improve RecentExpenses component styles for better responsiveness

--- src/components/dashboard/overview/RecentExpenses.tsx
+++ src/components/dashboard/overview/RecentExpenses.tsx
@@ -2,7 +2,7 @@ import RecentExpenseCard from "./RecentExpenseCard";
 
 const RecentExpenses = () => {
   return (
-    <>
+    <div className="border flex flex-col items-start gap-6 justify-start overflow-y-scroll max-h-[480px]  border-indigo-500/40 bg-indigo-950/30 w-2/5 p-4 rounded-md ">
       <h3 className="text-2xl">Recent Expenses</h3>
       <div className="w-full flex flex-col gap-2">
         <RecentExpenseCard
@@ -25,8 +25,23 @@ const RecentExpenses = () => {
           value={100}
           date={new Date().toLocaleDateString()}
         />
+        <RecentExpenseCard
+          title={"entertainment"}
+          value={100}
+          date={new Date().toLocaleDateString()}
+        />
+        <RecentExpenseCard
+          title={"entertainment"}
+          value={100}
+          date={new Date().toLocaleDateString()}
+        />
+        <RecentExpenseCard
+          title={"entertainment"}
+          value={100}
+          date={new Date().toLocaleDateString()}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
