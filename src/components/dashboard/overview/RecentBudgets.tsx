814fb66615e3774b0a225e456ae00c88ef9362f1
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-20 14:47:41 +0300

    refactor: Improve RecentBudgets component styles for better responsiveness

--- src/components/dashboard/overview/RecentBudgets.tsx
+++ src/components/dashboard/overview/RecentBudgets.tsx
@@ -3,7 +3,7 @@ import RecentBudgetCard from "./RecentBudgetCard";
 
 const RecentBudgets = () => {
   return (
-    <>
+    <div className="border flex flex-col p-4 bg-indigo-950/30 overflow-y-hidden items-start gap-2 justify-start border-indigo-500/40 w-full  rounded-md ">
       <h3 className="text-2xl">Recent Budgets</h3>
       <RecentBudgetCard title={"transport"} percentage={73} Icon={<Plane />} />
       <RecentBudgetCard title={"food"} percentage={40} Icon={<Banana />} />
@@ -12,7 +12,7 @@ const RecentBudgets = () => {
         percentage={60}
         Icon={<Drama />}
       />
-    </>
+    </div>
   );
 };
 
