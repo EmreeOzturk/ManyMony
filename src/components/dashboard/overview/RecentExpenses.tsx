d6b4f33a5aceb5642b9de4a1ea3e4c348bb3da33
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-20 19:19:48 +0300

    refactor: Update RecentExpenses component to dynamically render RecentExpenseCard based on data

--- src/components/dashboard/overview/RecentExpenses.tsx
+++ src/components/dashboard/overview/RecentExpenses.tsx
@@ -1,45 +1,22 @@
+import { Models } from "appwrite";
 import RecentExpenseCard from "./RecentExpenseCard";
 
-const RecentExpenses = () => {
+const RecentExpenses = ({ data }: { data: Models.Document[] }) => {
   return (
     <div className="border flex flex-col items-start gap-6 justify-start overflow-y-scroll max-h-[480px]  border-indigo-500/40 bg-indigo-950/30 w-2/5 p-4 rounded-md ">
       <h3 className="text-2xl">Recent Expenses</h3>
       <div className="w-full flex flex-col gap-2">
-        <RecentExpenseCard
-          title={"food"}
-          value={200}
-          date={new Date().toLocaleDateString()}
-        />
-        <RecentExpenseCard
-          title={"rent"}
-          value={400}
-          date={new Date().toLocaleDateString()}
-        />
-        <RecentExpenseCard
-          title={"transport"}
-          value={100}
-          date={new Date().toLocaleDateString()}
-        />
-        <RecentExpenseCard
-          title={"entertainment"}
-          value={100}
-          date={new Date().toLocaleDateString()}
-        />
-        <RecentExpenseCard
-          title={"entertainment"}
-          value={100}
-          date={new Date().toLocaleDateString()}
-        />
-        <RecentExpenseCard
-          title={"entertainment"}
-          value={100}
-          date={new Date().toLocaleDateString()}
-        />
-        <RecentExpenseCard
-          title={"entertainment"}
-          value={100}
-          date={new Date().toLocaleDateString()}
-        />
+        {data.map((expense) => {
+          return (
+            <RecentExpenseCard
+              key={expense.$id}
+              title={expense.name}
+              value={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
+        
       </div>
     </div>
   );
