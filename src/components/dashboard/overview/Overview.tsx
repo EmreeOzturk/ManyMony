782f0f8526e04c4b881ed102e4458a61563687bf
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-20 14:47:34 +0300

    refactor: Update Overview component with new chart data and remove unused code

--- src/components/dashboard/overview/Overview.tsx
+++ src/components/dashboard/overview/Overview.tsx
@@ -1,41 +1,11 @@
 // primary bg color = bg-indigo-950
 
-import { TrendingUp, TrendingDown, Wallet } from "lucide-react";
+import { TrendingUp, TrendingDown } from "lucide-react";
 import OverviewCard from "./OverviewCard";
 import RecentExpenses from "./RecentExpenses";
 import RecentBudgets from "./RecentBudgets";
 import { BarChart } from "@tremor/react";
-
-const chartdata = [
-  {
-    name: "Amphibians",
-    "Number of threatened species": 2488,
-  },
-  {
-    name: "Birds",
-    "Number of threatened species": 1445,
-  },
-  {
-    name: "Crustaceans",
-    "Number of threatened species": 743,
-  },
-  {
-    name: "Ferns",
-    "Number of threatened species": 281,
-  },
-  {
-    name: "Arachnids",
-    "Number of threatened species": 251,
-  },
-  {
-    name: "Corals",
-    "Number of threatened species": 232,
-  },
-  {
-    name: "Algae",
-    "Number of threatened species": 98,
-  },
-];
+import { chartdata2 } from "../analytic/Analytics";
 
 const dataFormatter = (number: number) =>
   Intl.NumberFormat("us").format(number).toString();
@@ -46,8 +16,8 @@ const Overview = () => {
         <div className="flex flex-col gap-2 w-full ">
           <div className="flex gap-2">
             <OverviewCard
-              title="Total Income"
-              value={400}
+              title="Total Budget"
+              value={1200}
               Icon={<TrendingUp className="text-green-400" />}
             />
             <OverviewCard
@@ -55,31 +25,22 @@ const Overview = () => {
               value={800}
               Icon={<TrendingDown className="text-red-400" />}
             />
-            <OverviewCard
-              title="Total Budget"
-              value={1200}
-              Icon={<Wallet className="text-sky-400" />}
-            />
           </div>
           <div className="border-indigo-500/40 bg-indigo-950/30 border h-full rounded-md">
             <BarChart
-              data={chartdata}
+              className="mt-6"
+              data={chartdata2}
               index="name"
-              categories={["Number of threatened species"]}
-              colors={["indigo-800"]}
+              categories={["Budget Limit", "Budget Spent"]}
+              colors={["blue", "teal", "amber", "rose", "indigo", "emerald"]}
               valueFormatter={dataFormatter}
-              onValueChange={(v) => console.log(v)}
-              className="w-full h-[99%]"
+              yAxisWidth={48}
             />
           </div>
         </div>
-        <div className="border flex flex-col items-start gap-2 justify-between border-indigo-500/40 bg-indigo-950/30 w-2/5 p-4 rounded-md ">
-          <RecentExpenses />
-        </div>
+        <RecentExpenses />
       </div>
-      <div className="border flex flex-col p-4 bg-indigo-950/30 overflow-y-hidden items-start gap-2 justify-start border-indigo-500/40 w-full  rounded-md ">
-        <RecentBudgets />
-      </div>
+      <RecentBudgets />
     </div>
   );
 };
