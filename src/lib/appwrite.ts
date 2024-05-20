41481c716643278c3636597ec9437572a665b456
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-20 17:17:58 +0300

    refactor: Update appwrite.ts to import Databases and create a new instance for better database management

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,7 +1,8 @@
-import { Client, Account } from "appwrite";
+import { Client, Account, Databases } from "appwrite";
 
 export const API_ENDPOINT = "https://cloud.appwrite.io/v1";
 export const PROJECT_ID = "662efab30012ddca3221";
 const client = new Client().setEndpoint(API_ENDPOINT).setProject(PROJECT_ID);
+export const databases = new Databases(client);
 export const account = new Account(client);
 export default client;
