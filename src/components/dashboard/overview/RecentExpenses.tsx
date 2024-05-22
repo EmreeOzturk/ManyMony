837e3bb1f1c894499cc730f406652504e5b46b28
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-22 15:58:40 +0300

    refactor: Update RecentExpenses component to use updated data structure and dynamically render RecentExpenseCard

--- src/components/dashboard/overview/RecentExpenses.tsx
+++ src/components/dashboard/overview/RecentExpenses.tsx
@@ -3,9 +3,9 @@ import RecentExpenseCard from "./RecentExpenseCard";
 
 const RecentExpenses = ({ data }: { data: Models.Document[] }) => {
   return (
-    <div className="border flex flex-col items-start justify-start overflow-y-scroll max-h-[480px]  border-indigo-500/40 bg-indigo-950/30 w-2/5 p-4 rounded-md ">
-      <h3 className="text-2xl">Recent Expenses</h3>
-      <div className="w-full my-auto justify-end flex flex-col gap-2 h-full">
+    <div className="border flex flex-col items-start justify-start  max-h-[480px]  overflow-y-auto border-indigo-500/40 bg-indigo-950/30 w-2/5 p-4 rounded-md ">
+      <h3 className="text-2xl mb-5">Recent Expenses</h3>
+      <div className="w-full my-auto justify-end flex flex-col gap-2">
         {data.map((expense) => {
           return (
             <RecentExpenseCard
@@ -16,7 +16,6 @@ const RecentExpenses = ({ data }: { data: Models.Document[] }) => {
             />
           );
         })}
-        
       </div>
     </div>
   );
