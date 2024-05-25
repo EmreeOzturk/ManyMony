583374904813692bde59c3f24f685dae70807e30
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-25 07:43:32 +0300

    refactor: Update BudgetDetail component to remove AddExpenseForm and ExpensesTable

--- src/pages/dashboard/BudgetDetail.tsx
+++ src/pages/dashboard/BudgetDetail.tsx
@@ -1,33 +1,31 @@
-import { useLoaderData } from "react-router-dom"
-import { Budget, Expense } from "../../types"
-import BudgetItem from "../../components/BudgetItem"
-import AddExpenseForm from "../../components/forms/AddExpenseForm"
-import ExpensesTable from "../../components/ExpensesTable"
+import { useLoaderData } from "react-router-dom";
+import { Budget, Expense } from "../../types";
+import BudgetItem from "../../components/BudgetItem";
+import ExpensesTable from "../../components/ExpensesTable";
 
 const BudgetDetail = () => {
-    const { budget, expenses } = useLoaderData() as { budget: Budget, expenses: Expense[] }
-    return (
-        <div
-            className="grid-lg"
-
-        >
-            <h1 className="h2">
-                <span className="accent">{budget.name}</span> Overview
-            </h1>
-            <div className="flex-lg">
-                <BudgetItem budget={budget} showDelete={true} />
-                <AddExpenseForm budgets={[budget]} />
-            </div>
-            {expenses && expenses.length > 0 && (
-                <div className="grid-md">
-                    <h2>
-                        <span className="accent">{budget.name}</span> Expenses
-                    </h2>
-                    <ExpensesTable expenses={expenses} />
-                </div>
-            )}
+  const { budget, expenses } = useLoaderData() as {
+    budget: Budget;
+    expenses: Expense[];
+  };
+  return (
+    <div className="grid-lg">
+      <h1 className="h2">
+        <span className="accent">{budget.name}</span> Overview
+      </h1>
+      <div className="flex-lg">
+        <BudgetItem budget={budget} showDelete={true} />
+      </div>
+      {expenses && expenses.length > 0 && (
+        <div className="grid-md">
+          <h2>
+            <span className="accent">{budget.name}</span> Expenses
+          </h2>
+          <ExpensesTable expenses={expenses} />
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default BudgetDetail
+export default BudgetDetail;
