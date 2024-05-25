1dc2969662a56343452e238fa9a9ca04a96a6357
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-25 07:49:09 +0300

    refactor: Remove unused console.log statements and optimize dashboardLoader

--- src/loaders/dashboardLoader.ts
+++ src/loaders/dashboardLoader.ts
@@ -2,16 +2,12 @@ import { databases } from "../lib/appwrite";
 import { Query } from "appwrite";
 import type { LoaderFunction } from "react-router-dom";
 import { fetchDataFromLocalStorage } from "../helper";
-export const dashboardLoader: LoaderFunction = async ({ context, request }) => {
+export const dashboardLoader: LoaderFunction = async () => {
   const userId = fetchDataFromLocalStorage("userId");
   const budgets = await databases.listDocuments(
     "66343e800011dbbdd0f4",
     "66343eb4001c491d89a7",
     [Query.equal("userId", userId), Query.orderDesc("$createdAt")]
   );
-  console.log(budgets);
-  console.log("dashboardLoader");
-  console.log(context);
-  console.log(request.headers);
   return budgets;
 };
