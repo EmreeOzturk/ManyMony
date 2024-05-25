f9f1cce47cd5cc4ce65dc62dcbb0460f29efea5a
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-25 07:45:31 +0300

    refactor: Update BudgetCard to display percentage without decimal places

--- src/components/dashboard/budgets/BudgetCard.tsx
+++ src/components/dashboard/budgets/BudgetCard.tsx
@@ -25,7 +25,7 @@ const BudgetCard = ({
       <Link to="/" className="relative flex flex-col cursor-pointer ">
         <div className="max-w-sm w-72 px-6 py-8 h-[270px] border bg-indigo-950/40 border-indigo-500/40 rounded-xl overflow-hidden shadow-lg">
           <div className="absolute top-0 right-0 px-3 py-3 rounded-bl-2xl border-zinc-600/40 border-l border-b  text-xs font-bold bg-zinc-900 text-zinc-300">
-            {percentage}%
+            {percentage.toFixed(0)}%
           </div>
           <div className="w-12 h-12 rounded-xl bg-zinc-900 p-2 flex items-center justify-center mb-2">
             <Album className="w-10 h-10 object-cover text-zinc-300" />
@@ -33,14 +33,7 @@ const BudgetCard = ({
           <div className="font-bold text-xl mb-2">{name}</div>
           <p className="text-zinc-300 text-lg">${amount}</p>
           <div className="flex items-center justify-start mt-6 gap-2">
-            {/* {categories.map((category) => (
-              <Badge className="px-4 py-2 text-sm" key={category}>
-                {category}
-              </Badge>
-            ))} */}
-            <Badge className="px-4 py-2 text-sm">
-              {categories}
-            </Badge>
+            <Badge className="px-4 py-2 text-sm">{categories}</Badge>
           </div>
         </div>
       </Link>
