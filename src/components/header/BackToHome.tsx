08663a666c1d6e96dde6c0b0781cc35ea27c6856
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-26 08:17:22 +0300

    refactor: Improve BackToHome component for better flexibility and reusability

--- src/components/header/BackToHome.tsx
+++ src/components/header/BackToHome.tsx
@@ -1,18 +1,24 @@
-import { Link } from 'react-router-dom'
-import LeftArrow from '../icons/LeftArrow'
+import { Link } from "react-router-dom";
+import LeftArrow from "../icons/LeftArrow";
 
-const BackToHome = () => {
-    return (
-        <div className="absolute top-10 left-10 cursor-pointer z-20">
-            <Link
-                to="/"
-                className="text-neutral-500 hover:text-neutral-300 transition-all flex items-center gap-1 justify-center ">
-                <LeftArrow />
-                Back To Home
+const BackToHome = ({
+  url = "/",
+  text = "Back To Home",
+}: {
+  url?: string;
+  text?: string;
+}) => {
+  return (
+    <div className="absolute top-10 left-10 cursor-pointer z-20">
+      <Link
+        to={url}
+        className="text-neutral-500 hover:text-neutral-300 transition-all flex items-center gap-1 justify-center "
+      >
+        <LeftArrow />
+        <span>{text}</span>
+      </Link>
+    </div>
+  );
+};
 
-            </Link>
-        </div>
-    )
-}
-
-export default BackToHome
+export default BackToHome;
