9acaae0feedc9c628dd22daa5a823d41bfb79c3d
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-26 09:10:50 +0300

    refactor: Update budgetDetailLoader to use appwrite database

--- src/loaders/budgetDetailLoader.ts
+++ src/loaders/budgetDetailLoader.ts
@@ -1,17 +1,19 @@
-import { findBudgetPropsById, findExpensesByBudgetId } from "../helper";
 import { LoaderFunction } from "react-router-dom";
+import { fetchDataFromLocalStorage } from "../helper";
+import { databases } from "../lib/appwrite";
 export const budgetDetailLoader: LoaderFunction = async ({ params }) => {
-  const budget = findBudgetPropsById(params.budgetId as string);
-  if (!budget) {
-    return {
-      status: 404,
-    };
+  const userId = fetchDataFromLocalStorage("userId");
+  if (!userId) {
+    throw new Error("User not found");
   }
-
-  const expenses = findExpensesByBudgetId(params.budgetId as string);
-
-  return {
-    budget,
-    expenses,
-  };
+  const budgetId = params.budgetId;
+  const budget = await databases.getDocument(
+    "66343e800011dbbdd0f4",
+    "66343eb4001c491d89a7",
+    budgetId?.toString() as string
+  );
+  if (budget.userId !== userId) {
+    throw new Error("Unauthorized");
+  }
+  return budget;
 };
