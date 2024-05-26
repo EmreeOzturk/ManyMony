c58055dea7aaef9650299b9a0d9faa01162fad07
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-27 00:25:58 +0300

    refactor: Delete budget in budgetDetailAction

--- src/actions/budgetDetailAction.ts
+++ src/actions/budgetDetailAction.ts
@@ -1,4 +1,5 @@
 import type { ActionFunction } from "react-router-dom";
+import { redirect } from "react-router-dom";
 import { databases } from "../lib/appwrite";
 import { toast } from "react-toastify";
 
@@ -12,25 +13,34 @@ export const budgetDetailAction: ActionFunction = async ({
 
   if (actionType === "deleteBudget") {
     const budgetId = formData.get("budgetId");
-    const userId = formData.get("userId");
+    console.log(budgetId);
 
-    databases
-      .deleteDocument(
-        "66343e800011dbbdd0f4",
-        "66343eb4001c491d89a7",
-        budgetId as string
-      )
-      .then(() => {
-        toast.success("Budget deleted successfully", {
-          autoClose: 2000,
-          position: "bottom-right",
+    try {
+      console.log("Deleting budget");
+      databases
+        .deleteDocument(
+          "66343e800011dbbdd0f4",
+          "66343eb4001c491d89a7",
+          budgetId as string
+        )
+        .then(() => {
+          toast.success("Budget deleted successfully", {
+            autoClose: 2000,
+            position: "bottom-right",
+          });
+        })
+        .catch(() => {
+          toast.error("Failed to delete budget", {
+            autoClose: 2000,
+            position: "bottom-right",
+          });
         });
-      })
-      .catch(() => {
-        toast.error("Failed to delete budget", {
-          autoClose: 2000,
-          position: "bottom-right",
-        });
-      });
+      return redirect("/dashboard");
+    } catch (e) {
+      console.log(e);
+      return {
+        status: "failed",
+      };
+    }
   }
 };
