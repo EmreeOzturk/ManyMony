5f29a0205588a5c747d0c073c01a3e79c09a7081
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-26 08:05:47 +0300

    refactor: Add loading state to delete button in TransactionTableRow component

--- src/components/dashboard/transactions/TransactionTableRow.tsx
+++ src/components/dashboard/transactions/TransactionTableRow.tsx
@@ -1,10 +1,11 @@
-import { Ellipsis } from "lucide-react";
+import { Ellipsis, LoaderIcon } from "lucide-react";
 import { TableCell } from "../../ui/table";
 import { motion } from "framer-motion";
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import { Button } from "../../ui/button";
-import { Edit, Trash } from "lucide-react";
-import { Form } from "react-router-dom";
+import { Trash } from "lucide-react";
+import { Form, useActionData } from "react-router-dom";
+import { useState } from "react";
 import {
   Popover,
   PopoverContent,
@@ -31,6 +32,15 @@ const TransactionTableRow = ({
   budgetId: string;
   usage: string;
 }) => {
+  const [loading, setLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const data = useActionData() as { status: string };
+  useEffect(() => {
+    if (data && data?.status === "success") {
+      setLoading(false);
+      setIsOpen(false);
+    }
+  }, [data]);
   return (
     <motion.tr
       initial={{ opacity: 0, y: 100 }}
@@ -52,27 +62,56 @@ const TransactionTableRow = ({
       </TableCell>
       <TableCell className="text-right">${amount}</TableCell>
       <TableCell className="flex justify-end pr-6">
-        <Popover>
+        {/* <Form method="POST" className=" inline-block">
+          <input type="hidden" name="_action" value="deleteExpense" />
+          <input type="hidden" name="_id" value={id} />
+          <input type="hidden" name="_budgetId" value={budgetId} />
+          <input type="hidden" name="amount" value={amount} />
+          <input type="hidden" name="usage" value={usage} />
+          <Button className="text-xs" variant={"destructive"}>
+            <Trash className="w-4 h-4 mr-2" />
+            Delete
+          </Button>
+        </Form> */}
+        <Popover open={isOpen} onOpenChange={setIsOpen}>
           <PopoverTrigger>
             <Ellipsis />
           </PopoverTrigger>
-          <PopoverContent className="w-56 space-x-2  px-4 bg-zinc-900">
-            <Button
+          <PopoverContent className="w-full  bg-zinc-900">
+            {/* <Button
               className="text-xs bg-transparent text-zinc-300 "
               variant={"outline"}
             >
               <Edit className="w-4 h-4 mr-2" />
               Edit
-            </Button>
-            <Form method="POST" className=" inline-block">
+            </Button> */}
+            <Form
+              onSubmit={() => {
+                setLoading(true);
+              }}
+              method="POST"
+              className=" inline-block"
+            >
               <input type="hidden" name="_action" value="deleteExpense" />
               <input type="hidden" name="_id" value={id} />
               <input type="hidden" name="_budgetId" value={budgetId} />
               <input type="hidden" name="amount" value={amount} />
               <input type="hidden" name="usage" value={usage} />
-              <Button className="text-xs" variant={"destructive"}>
-                <Trash className="w-4 h-4 mr-2" />
-                Delete
+              <Button
+                disabled={loading}
+                className="text-xs"
+                variant={"destructive"}
+              >
+                {loading ? (
+                  <span>
+                    <LoaderIcon className="animate-spin h-5 w-5" />
+                  </span>
+                ) : (
+                  <>
+                    <Trash className="w-4 h-4 mr-2" />
+                    Delete
+                  </>
+                )}
               </Button>
             </Form>
           </PopoverContent>
