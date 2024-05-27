9102053c1b8566c4da0373c2c0a55b6a057b030b
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-27 10:58:35 +0300

    refactor: Update ModalTriggerButton to use a div instead of a button

--- src/components/dashboard/ModalTriggerButton.tsx
+++ src/components/dashboard/ModalTriggerButton.tsx
@@ -1,10 +1,10 @@
 const ModalTriggerButton = ({ text }: { text: string }) => {
   return (
-    <button className="border flex items-center border-zinc-800 hover:scale-105 transition-all group hover:border-indigo-300 justify-center rounded-md px-4 py-2">
+    <div className="border flex items-center border-zinc-800 hover:scale-105 transition-all group hover:border-indigo-300 justify-center rounded-md px-4 py-2">
       <span className="relative block text-zinc-600 group-hover:text-indigo-300">
         {text}
       </span>
-    </button>
+    </div>
   );
 };
 
