0bc716c6dffa4added705c7b48312b646cad0f82
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-28 02:09:10 +0300

    refactor: Update Error component to use CSS classes for styling and fix formatting

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,31 +1,53 @@
-import { Link, useRouteError, useNavigate } from "react-router-dom"
+import { Link, useRouteError, useNavigate } from "react-router-dom";
 
 type ErrorType = {
-  statusText: string
-  status: number
-  message: string
-}
+  statusText: string;
+  status: number;
+  message: string;
+};
 
 const Error = () => {
-  const navigate = useNavigate()
-  const error: ErrorType = useRouteError() as ErrorType
-  console.log({ error })
+  const navigate = useNavigate();
+  const error: ErrorType = useRouteError() as ErrorType;
+  console.log({ error });
   return (
-    <div
-      className="error"
-    >
+    <div className="text-zinc-300 text-center flex items-center justify-center flex-col gap-4 h-screen w-screen bg-zinc-900">
       <h1>Oops! Something went wrong</h1>
       <p>{error?.statusText || error?.status || error.message}</p>
-      <div className="flex-md">
-        <button className="btn btn--dark" onClick={
-          () => navigate(-1)
-        }>
+      <div className="flex gap-4">
+        <button
+          className="
+            text-white
+            bg-indigo-500
+            px-4
+            py-2
+            rounded-md
+            hover:bg-indigo-600
+            transition
+            duration-200
+            "
+          onClick={() => navigate(-1)}
+        >
           <span>Go back</span>
         </button>
-        <Link to="/" className="btn btn--dark"><span>Go home</span></Link>
+        <Link
+          to="/"
+          className="
+            text-white
+            bg-indigo-500
+            px-4
+            py-2
+            rounded-md
+            hover:bg-indigo-600
+            transition
+            duration-200
+        "
+        >
+          <span>Go home</span>
+        </Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default Error;
