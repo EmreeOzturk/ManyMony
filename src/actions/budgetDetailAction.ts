4a17fa7b1ed9d8695faa599ae07d5a4d7df10dc6
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-27 14:31:04 +0300

    refactor: Add deleteExpense functionality to budgetDetailAction

--- src/actions/budgetDetailAction.ts
+++ src/actions/budgetDetailAction.ts
@@ -2,6 +2,8 @@ import type { ActionFunction } from "react-router-dom";
 import { redirect } from "react-router-dom";
 import { databases } from "../lib/appwrite";
 import { toast } from "react-toastify";
+import { checkDeleteExpenseFormData } from "../helper/checkFormData";
+import { AppwriteException } from "appwrite";
 
 export const budgetDetailAction: ActionFunction = async ({
   request,
@@ -40,6 +42,59 @@ export const budgetDetailAction: ActionFunction = async ({
       console.log(e);
       return {
         status: "failed",
+      };
+    }
+  }
+  if (actionType === "deleteExpense") {
+    const expenseId = formData.get("_id");
+    const budgetId = formData.get("_budgetId");
+    const amount = formData.get("amount");
+    const usage = formData.get("usage");
+
+    const errors = checkDeleteExpenseFormData(
+      expenseId as string,
+      amount as string,
+      budgetId as string
+    );
+    if (Object.keys(errors).length > 0) {
+      return errors;
+    }
+    try {
+      const deleteExpense = databases.deleteDocument(
+        "66343e800011dbbdd0f4",
+        "66343ebc0025e1be1729",
+        expenseId?.toString() as string
+      );
+      const updateBudget = databases.updateDocument(
+        "66343e800011dbbdd0f4",
+        "66343eb4001c491d89a7",
+        budgetId as string,
+        {
+          usage: Number(usage) - Number(amount),
+        }
+      );
+      await Promise.all([deleteExpense, updateBudget])
+        .then(() => {
+          toast.success("Expense deleted successfully", {
+            autoClose: 2000,
+            position: "bottom-right",
+          });
+        })
+        .catch((error: AppwriteException) => {
+          console.log(error);
+          return {
+            message: "Failed to delete expense",
+            status: "error",
+          };
+        });
+      return {
+        message: "Expense deleted successfully",
+        status: "success",
+      };
+    } catch (error) {
+      return {
+        message: "Failed to delete expense",
+        status: "error",
       };
     }
   }
