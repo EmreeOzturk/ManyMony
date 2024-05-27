55fa997218532e9ed761c673173437278d327c4e
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-27 14:16:47 +0300

    refactor: Update RecentBudgetCard component to improve layout and navigation

--- src/components/dashboard/overview/RecentBudgetCard.tsx
+++ src/components/dashboard/overview/RecentBudgetCard.tsx
@@ -15,7 +15,7 @@ const RecentBudgetCard = ({
   const navigate = useNavigate();
   return (
     <div className="shadow-indigo-200 border-b h-1/4 rounded-md hover:bg-indigo-950/70 w-full flex items-center justify-between px-8 ">
-      <div className="flex items-center gap-2 w-20">
+      <div className="flex items-center justify-start  gap-2 w-20">
         <div className="bg-zinc-700 p-[3px] rounded-sm">{Icon}</div>
         <p className="text-[16px] text-zinc-300">{title}</p>
       </div>
@@ -29,12 +29,12 @@ const RecentBudgetCard = ({
         </div>
         <span className="text-sm">{percentage.toFixed(0)}%</span>
       </div>
-      <div className="cursor-pointer">
+      <div className="cursor-pointer flex items-center justify-center hover:scale-110 transition-all">
         <button
           onClick={() => {
             navigate(`/budgets/${budgetId}`);
           }}
-          className="text-lg font-normal text-blue-400 underline"
+          className="text-lg font-normal text-blue-400 "
         >
           View
         </button>
