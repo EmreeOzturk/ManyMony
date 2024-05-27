3bf21c3be589acee0a4088cfda8db84bae001306
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-27 14:16:56 +0300

    refactor: Adjust max height and border styles in RecentExpenses component

--- src/components/dashboard/overview/RecentExpenses.tsx
+++ src/components/dashboard/overview/RecentExpenses.tsx
@@ -3,7 +3,7 @@ import RecentExpenseCard from "./RecentExpenseCard";
 
 const RecentExpenses = ({ data }: { data: Models.Document[] }) => {
   return (
-    <div className="border flex flex-col items-start justify-start  max-h-[500px]  overflow-y-auto border-indigo-500/40 bg-indigo-950/30 w-2/5 p-4 rounded-md ">
+    <div className="border flex flex-col items-start justify-start  max-h-[460px]  overflow-y-auto border-indigo-500/40 bg-indigo-950/30 w-2/5 p-4 rounded-md ">
       <h3 className="text-2xl mb-5">Recent Expenses</h3>
       <div className="w-full my-auto justify-end flex flex-col gap-2">
         {data.map((expense) => {
