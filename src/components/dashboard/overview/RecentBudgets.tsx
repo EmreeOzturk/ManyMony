335c76216177ee891856740424ee4eccc84bd2d6
Emre Ozturk <emreeozturk42@gmail.com>
2024-05-27 14:13:30 +0300

    refactor: Update RecentBudgets component to adjust height and border styles

--- src/components/dashboard/overview/RecentBudgets.tsx
+++ src/components/dashboard/overview/RecentBudgets.tsx
@@ -12,7 +12,7 @@ const RecentBudgets = ({
   }[];
 }) => {
   return (
-    <div className="border flex flex-col p-4 bg-indigo-950/30 overflow-y-hidden items-start gap-2 justify-start border-indigo-500/40 w-full  rounded-md ">
+    <div className="border flex flex-col p-2 bg-indigo-950/30 overflow-y-auto items-start h-full gap-2 justify-start border-indigo-500/40 w-full  rounded-md ">
       <h3 className="text-2xl">Recent Budgets</h3>
       {data.map((budget) => {
         return (
